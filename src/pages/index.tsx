a7b786fe0a91cf6026102281892e6cb0b34c367f
gabrielfelipeio <gabrisilvas@outlook.com>
2021-05-10 22:27:48 -0300

    add ServerSideRendering and StaticSiteGenerator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,33 @@
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   );
+}
+
+// SERVER SIDE RENDERING
+//export async function getServerSideProps() {
+//  const response = await fetch('http://localhost:3333/episodes');
+//  const data = await response.json();
+//
+//  return {
+//    props: {
+//      episodes: data
+//    }
+//  }
+//}
+
+// STATIC SITE GENERATOR
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }

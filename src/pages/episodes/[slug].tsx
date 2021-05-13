e283ae7263ff9c4c99e4251d405e95fdf990de36
gabrielfelipeio <gabrisilvas@outlook.com>
2021-05-12 21:27:10 -0300

    add Context API, add Player Context, do play on episodes, show player infos

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,6 +3,8 @@ import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContext';
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
@@ -25,6 +27,7 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+    const { play } = useContext(PlayerContext);
 
     return (
         <div>
@@ -41,7 +44,7 @@ export default function Episode({ episode }: EpisodeProps) {
                         src={episode.thumbnail}
                         objectFit="cover"
                     />
-                    <button type="button">
+                    <button type="button" onClick={() => play(episode)}>
                         <img src="/play.svg" alt="Tocar episódio" />
                     </button>
                 </div>
@@ -60,8 +63,25 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+
+    const { data } = await api.get('episodes', {
+        params: {
+            _limit: 2,
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    });
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    });
+
     return {
-        paths: [],
+        paths,
         fallback: 'blocking'
     }
 };

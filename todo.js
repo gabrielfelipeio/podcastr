943566c6dd5207e9430f05b07e006597e4c83ab0
gabrielfelipeio <gabrisilvas@outlook.com>
2021-05-13 00:02:25 -0300

    add TODO list to execute

---
+++ todo.js
@@ -0,0 +1,6 @@
+// TODO: Criar um README.md para opensource;
+// TODO: Transformar em site Responsivo (Responsividade);
+// TODO: PWA (next-pwa);
+// TODO: Tema Dark (exemplo: github.com/getomni);
+// TODO: Electron;
+// TODO: App Desktop;
